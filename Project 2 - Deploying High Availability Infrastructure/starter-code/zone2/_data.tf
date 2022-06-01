43425c5841580444629381a380c793af79922c84
Björn Gam <bjoerngam@gmail.com>
2022-06-01 20:22:34 +0200

    Add files via upload

--- Project 2 - Deploying High Availability Infrastructure/starter-code/zone2/_data.tf
+++ Project 2 - Deploying High Availability Infrastructure/starter-code/zone2/_data.tf
@@ -19,7 +19,7 @@ data "aws_caller_identity" "current" {}
 data "terraform_remote_state" "vpc" {
   backend = "s3" 
   config = {
-     bucket = "udacity-tf-bgam-west"
+     bucket = "udacity-tf-bgam"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
